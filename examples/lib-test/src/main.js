1ab19b93fc9c94b254e7df7af23658200081990c
Arne van Iterson <arne@arnweb.nl>
2020-02-29 08:27:07 +0100

    Added some examples

---
+++ examples/lib-test/src/main.js
@@ -0,0 +1,48 @@
+import asdf from "../../../asdf/index.js";
+const { Game, Container, CanvasRenderer, math, KeyControls, MouseControls, Text, Texture, Sprite } = asdf;
+
+const game = new Game(640, 320, false);
+const { scene, w, h } = game;
+
+const buildings = scene.add(new Container());
+const makeRandom = (b, x) => {
+  b.scale.x = math.randf(1,3);
+  b.scale.y = math.randf(1,3);
+  b.pos.x = x;
+  b.pos.y = h - b.scale.y * 64;
+};
+
+for (let x = 0; x < 50; x++) {
+  const b = buildings.add(new Sprite(new Texture("res/images/building.png")));
+  makeRandom(b, math.rand(w));
+}
+
+var rotation = 0;
+const ship = new Sprite(new Texture("res/images/spaceship.png"));
+ship.pivot = { x: 16, y: 16 };
+scene.add(ship);
+
+
+game.run((dt ,t) => {
+  ship.update = function(dt, t) {
+    const {scale} = this;
+    scale.x = Math.abs(Math.sin(t)) + 1;
+    scale.y = Math.abs(Math.sin(t)) + 1;
+
+    rotation = rotation + 0.2;
+    ship.rotation = rotation; 
+
+    ship.pos.y = (Math.abs(Math.sin(t))) * 50 + (h / 2) - 16;
+    ship.pos.x += dt * 50;
+    if (ship.pos.x > w) {
+      ship.pos.x = -32;
+    }
+  }
+
+  buildings.map(b => {
+    b.pos.x -= 100 * dt;
+    if (b.pos.x < -80) {
+      makeRandom(b,w);
+    }
+  });
+});
---
+++ examples/tilemap-test/src/main.js
@@ -0,0 +1,28 @@
+import asdf from "../../../asdf/index.js";
+const { Game, Container, CanvasRenderer, math, KeyControls, MouseControls, Text, Texture, Sprite, TileSprite, TileMap, TileMapXML, SpriteSheetXML } = asdf;
+
+const game = new Game(640, 320, true);
+const { scene, w, h } = game;
+
+const mouseAim = new MouseControls();
+
+const tanksTexture = new Texture("./res/images/allSprites_default.png")
+const tanksXml = new SpriteSheetXML("./res/images/allSprites_default.xml");
+const map = { x: 640 / 64, y: 320 / 64 };
+
+// Make a random level of tile indexes
+const level = [
+    135, 135, 135, 135, 135, 135, 135, 135, 135, 135,
+    135, 135, 135, 135, 135, 135, 135, 135, 135, 135,
+    158, 158, 158, 158, 158, 158, 158, 158, 158, 158,
+    160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
+    160, 160, 160, 160, 160, 160, 160, 160, 160, 160,
+];
+
+scene.add(new TileMapXML(level, map.x, map.y, tanksTexture, tanksXml));
+
+game.run((dt, t) => {
+    if (mouseAim.isDown) {
+        console.log('cliccccccccccc');
+    }
+});
